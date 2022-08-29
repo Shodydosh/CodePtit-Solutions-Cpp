8e3eef6678e9a96997c9d0cef325fe3ddce74536
Wh0 <87895460+Shodydosh@users.noreply.github.com>
2022-08-30 01:00:41 +0700

    Create CPP0274.cpp

---
+++ CPP0274.cpp
@@ -0,0 +1,92 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define endl '\n'
+#define chk cout << "<< * checked  * >>" << endl;
+#define db cout << '<' << "i" << '=' << i << '>' << endl
+
+using namespace std;
+
+vector<long long> s(1000000, 0);
+void sieve (long long n) {
+	s[0] = 1, s[1] = 1;
+	for (long long i=2; i<=n; i++){
+        if(s[i] == 0){
+            for (long long j=i+i; j<=n; j+=i) {
+                s[j] = 1;
+            }
+        }
+	}
+}
+
+long long checkNT(long long n){
+    if(n <= 1) return 0;
+    if(n <= 3) return 1;
+    if(n % 2 == 0 || n % 3 == 0) return 0;
+    for(long long i=2; i<=sqrt(n); i++){
+        if(n % i == 0) return 0;
+    }
+    return 1;
+}
+
+long long calSum(long long n){
+	long long m = n, sum = 0;
+	while (m != 0) {
+		sum += m%10;
+		m /= 10;
+	}
+	return sum;
+}
+
+long long BCNN(long long a, long long b){
+    return a*b / __gcd(a, b);
+}
+
+long long cal(long long x, long long a){
+    long long ans = x;
+    long long tmp = 0;
+    
+    for(int i=0; i<=20; i++){
+        if(pow(10, i) > x){
+            tmp = i;
+            break;
+        }
+    }
+
+    a--;
+    while(a--){
+        ans = ans*pow(10, tmp)+x;
+    }
+
+    // cout << ans << endl;
+    return ans;
+}
+
+long long gcd(long long x,long long y){ 
+    if (y==0) return x; 
+    return gcd(y,x%y); 
+}
+
+int main(){
+ 	ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+	int t;
+	cin >> t;
+	while(t--){
+        vector<int> a(1000001, 0);
+        long long n; cin >> n;
+        long long cnt = 0;
+        for(int i=0; i<n; i++){
+            int tmp; cin >> tmp;
+            if(a[tmp] == 0) a[tmp] = 1;
+            else if(a[tmp] == 1){
+                cnt += 2;
+                a[tmp] = 2;
+            }
+            else cnt += 1;
+        }
+        cout << cnt << endl;
+	}
+ 	return 0;
+}
