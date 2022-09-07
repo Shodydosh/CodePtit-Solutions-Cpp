7e84f567941928e5400f9a2e1259ea79cfdf3066
Wh0 <87895460+Shodydosh@users.noreply.github.com>
2022-09-08 01:44:20 +0700

    Create CPP0335.cpp

---
+++ CPP0335.cpp
@@ -0,0 +1,49 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define endl '\n'
+#define chk cout << "<< * checked  * >>" << endl;
+#define db cout << '<' << "i" << '=' << i << '>' << endl
+
+using namespace std;
+
+long long calculate(string a)
+{
+    long long ans = 0, tmp = 0, temp = 0, max = 0;
+    for (int i = a.size() - 1; i >= 0; i--)
+    {
+        if (a[i] >= '0' && a[i] <= '9')
+        {
+            temp += (a[i] - '0') * pow(10, tmp);
+            tmp++;
+        }
+        else
+        {
+            // cout << temp << ' ';
+            if (temp > max)
+                max = temp;
+            temp = 0;
+            tmp = 0;
+        }
+    }
+    if (temp > max)
+        max = temp;
+    return max;
+}
+
+int main()
+{
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
+    cout.tie(0);
+    long long t;
+    cin >> t;
+    cin.ignore();
+    while (t--)
+    {
+        string a;
+        cin >> a;
+        cout << calculate(a) << endl;
+    }
+    return 0;
+}
