89e4c9fd44b8bd6ecc5e856f78c7c4e9ad7d2bdb
Wh0 <87895460+Shodydosh@users.noreply.github.com>
2022-09-23 10:59:29 +0700

    Create CPP0416.cpp

---
+++ CPP0416.cpp
@@ -0,0 +1,89 @@
+#include <bits/stdc++.h>
+
+#define ll long long
+#define endl '\n'
+#define chk cout << "<< * checked  * >>" << endl;
+#define db cout << '<' << "i" << '=' << i << '>' << endl
+
+using namespace std;
+
+// struct SinhVien
+// {
+//     string name;
+//     string maSV;
+//     string dob;
+//     double gpa;
+// };
+
+// void nhapThongTinSV(SinhVien ds[], int N)
+// {
+//     for (int i = 0; i < N; i++)
+//     {
+//         cin.ignore();
+//         getline(cin, ds[i].name);
+//         cin >> ds[i].maSV;
+//         cin >> ds[i].dob;
+//         cin >> ds[i].gpa;
+
+//         string tmp = ds[i].dob;
+//         if (tmp[1] == '/')
+//             tmp = '0' + tmp;
+//         if (tmp[4] == '/')
+//             tmp.insert(3, "0");
+//         ds[i].dob = tmp;
+//     }
+// }
+
+// void inThongTinSV(SinhVien ds[], int N)
+// {
+//     for (int i = 0; i < N; i++)
+//     {
+//         cout << "B20DCCN";
+
+//         int tmp = i + 1;
+//         if (tmp < 10)
+//             cout << "00" << tmp;
+//         else if (tmp < 100)
+//             cout << "0" << tmp;
+//         else
+//             cout << tmp;
+//         cout << ' ';
+
+//         cout << ds[i].name << ' ';
+//         cout << ds[i].maSV << ' ';
+//         cout << ds[i].dob << ' ';
+//         cout << setprecision(2) << ds[i].gpa << endl;
+//     }
+// }
+
+int main()
+{
+    ios_base::sync_with_stdio(0);
+    cin.tie(NULL);
+    cout.tie(NULL);
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        long long m, n;
+        cin >> m >> n;
+        vector<long long> v1(m);
+        for (int i = 0; i < m; i++)
+        {
+            cin >> v1[i];
+        }
+
+        int count = 0;
+
+        for (int i = 0; i < m - 1; i++)
+        {
+            for (int j = i + 1; j < m; j++)
+            {
+                if (v1[i] + v1[j] == n)
+                    count++;
+            }
+        }
+
+        cout << count << endl;
+    }
+}
