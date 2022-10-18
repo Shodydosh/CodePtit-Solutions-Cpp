38af6e6eaa6aec6a9fe028d2b072ac349edc06ed
Wh0 <87895460+Shodydosh@users.noreply.github.com>
2022-10-18 16:59:49 +0700

    Create CPP0528.cpp

---
+++ CPP0528.cpp
@@ -0,0 +1,58 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+struct sinhVien
+{
+    int stt;
+    string maSV, name, maLop, email, cty;
+};
+
+bool cmp(sinhVien a, sinhVien b)
+{
+    return a.name < b.name;
+}
+
+int main()
+{
+    int n;
+    cin >> n;
+    sinhVien sv[n];
+    for (int i = 0; i < n; i++)
+    {
+        cin.ignore();
+        getline(cin, sv[i].maSV);
+        getline(cin, sv[i].name);
+        getline(cin, sv[i].maLop);
+        cin >> sv[i].email;
+        cin >> sv[i].cty;
+        sv[i].stt = i + 1;
+    }
+    int m;
+    cin >> m;
+
+    string company[m];
+    for (int i = 0; i < m; i++)
+    {
+        cin >> company[i];
+    }
+
+    sort(sv, sv + n, cmp);
+
+    for (int j = 0; j < m; j++)
+    {
+        for (int i = 0; i < n; i++)
+        {
+            string a = company[j];
+            if (sv[i].cty == a)
+            {
+                cout << sv[i].stt << ' ';
+                cout << sv[i].maSV << ' ';
+                cout << sv[i].name << ' ';
+                cout << sv[i].maLop << ' ';
+                cout << sv[i].email << ' ';
+                cout << sv[i].cty << endl;
+            }
+        }
+    }
+}
