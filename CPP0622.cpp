b0e84d7540baba357e766da19fb20c646a8e70fe
Wh0 <87895460+Shodydosh@users.noreply.github.com>
2022-10-26 03:19:08 +0700

    Create CPP0622.cpp

---
+++ CPP0622.cpp
@@ -0,0 +1,69 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class SinhVien
+{
+private:
+    string b, d;
+
+public:
+    string c, a;
+    SinhVien()
+    {
+        a = b = c = d = "";
+    }
+    friend istream &operator>>(istream &mycin, SinhVien &);
+    friend ostream &operator<<(ostream &mycout, SinhVien);
+};
+int t = 0;
+istream &operator>>(istream &mycin, SinhVien &x)
+{
+    getline(mycin, x.a);
+    getline(mycin, x.b);
+    getline(mycin, x.c);
+    getline(mycin, x.d);
+    return mycin;
+}
+ostream &operator<<(ostream &mycout, SinhVien x)
+{
+    mycout << x.a << " " << x.b << " " << x.c << " " << x.d << endl;
+    return mycout;
+}
+bool cmp(SinhVien X, SinhVien Y)
+{
+    if (X.c < Y.c)
+        return true;
+    if (X.c == Y.c && X.a < Y.a)
+        return true;
+    return false;
+}
+void sapxep(SinhVien *ds, int n)
+{
+    sort(ds, ds + n, cmp);
+}
+int main()
+{
+    SinhVien ds[1000];
+    int n;
+    cin >> n;
+    cin.ignore();
+    for (int i = 0; i < n; i++)
+        cin >> ds[i];
+
+    int m;
+    cin >> m;
+    cin.ignore();
+
+    for (int j = 0; j < m; j++)
+    {
+        string s;
+        cin >> s;
+        cout << "DANH SACH SINH VIEN LOP " << s << ':' << endl;
+        for (int i = 0; i < n; i++)
+        {
+            if (ds[i].c == s)
+                cout << ds[i];
+        }
+    }
+}
